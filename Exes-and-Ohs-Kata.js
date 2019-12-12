3c039e2392a7ccf4c1f3bca6d838c2087f96f936
Gustavo Rivera PG <grpecunia@gmail.com>
2019-12-12 06:32:50 -0500

    Add files via upload

---
+++ Exes-and-Ohs-Kata.js
@@ -0,0 +1,19 @@
+function XO(str) {
+console.log(str)
+if (str.length == 0) {
+  return true;
+}
+  var str = str.toLowerCase();
+  var xString = 'x';
+  var oString = 'o';
+  var xCount = (str.match(/x/g) || []).length;
+  var oCount = (str.match(/o/g) || []).length;
+  console.log('x count is', xCount)
+  console.log('o count is', oCount)
+  for (let i = 0; i < str.length; i++) 
+  if (xCount === oCount) {
+    return true;
+    } else {
+    return false;
+    }
+  }
